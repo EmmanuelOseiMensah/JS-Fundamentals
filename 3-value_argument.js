768c46e7322bdf6fc29d5162788cc5c91a958a2f
EmmanuelOseiMensah <accaoseimensah4christ32@gmail.com>
2025-11-09 17:32:07 +0000

    added to include HBTN argument

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,5 +1,13 @@
-const firstArgument = process.argv[2];
-if (firstArgument === undefined) {
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+
+if (arg1 === undefined) {
     console.log("No argument");
-} 
+}else if (arg2 ===undefined) {
+    console.log(`One argument: ${arg1}`);
+} else {
+    console.log(`Two arguments:  ${arg1} ${arg2}`);
+
+}
 
