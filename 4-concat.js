a7de4b602af6b429177a40f08ce25baf7746f213
EmmanuelOseiMensah <accaoseimensah4christ32@gmail.com>
2025-11-09 21:38:23 +0000

    adding two numbers

--- 4-concat.js
+++ 4-concat.js
@@ -1,7 +1,15 @@
 // Get the first and second arguments from the command line
+//const firstArg = process.argv[2];
+//const secondArg = process.argv[3];
+
+// Print them in the requested format
+//console.log(firstArg + " is " + secondArg);
+
+// Get the first and second arguments from the command line
 const firstArg = process.argv[2];
 const secondArg = process.argv[3];
 
-// Print them in the requested format
-console.log(firstArg + " is " + secondArg);
+// Print them using template literals
+console.log(`${firstArg} is ${secondArg}`);
+
 
--- 9-add.js
+++ 9-add.js
@@ -1,15 +1,32 @@
-const firstInput = process.argv[2];
-const secondInput = process.argv[3];
-const a = parseInt(firstInput);
-const b = parseInt(secondInput);
+//const firstInput = process.argv[2];
+//const secondInput = process.argv[3];
+//const a = parseInt(firstInput);
+//const b = parseInt(secondInput);
 
+//function add(a, b) {
+//  return a + b;
+//}
+
+//if (isNaN(a) || isNaN(b)) {
+//  console.log("Please provide two numbers");
+//} else {
+//  console.log(add(a, b));
+//}
+
+// Get the first and second arguments from the command line
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+// Convert them to integers
+const a = parseInt(firstArg);
+const b = parseInt(secondArg);
+
+// Define the add function
 function add(a, b) {
   return a + b;
 }
 
-if (isNaN(a) || isNaN(b)) {
-  console.log("Please provide two numbers");
-} else {
-  console.log(add(a, b));
-}
+// Print the result of the addition
+console.log(add(a, b));
+
 
