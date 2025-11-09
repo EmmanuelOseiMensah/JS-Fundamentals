c5385372411a11c34d12d7236c9136db565600f4
EmmanuelOseiMensah <accaoseimensah4christ32@gmail.com>
2025-11-09 21:20:05 +0000

    Modification of previous code

--- 4-concat.js
+++ 4-concat.js
@@ -1,12 +1,7 @@
-// Get the first two potential user arguments.
-// arg1 will be the first argument, or undefined if missing.
-const arg1 = process.argv[2];
+// Get the first and second arguments from the command line
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
 
-// arg2 will be the second argument, or undefined if missing.
-const arg2 = process.argv[3];
+// Print them in the requested format
+console.log(firstArg + " is " + secondArg);
 
-// Use string concatenation (+) to join arg1, " is ", and arg2.
-// When arg1 or arg2 is 'undefined', JavaScript converts it to the string "undefined"
-// which matches the required output behavior.
-console.log(arg1 + " is " + arg2);
-    
