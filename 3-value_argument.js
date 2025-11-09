ad219cbf62b836b67d73640ee1e5f8028de23c62
EmmanuelOseiMensah <accaoseimensah4christ32@gmail.com>
2025-11-09 21:30:20 +0000

    used third value argument

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,13 +1,24 @@
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
+//const arg1 = process.argv[2];
+//const arg2 = process.argv[3];
 
 
-if (arg1 === undefined) {
-    console.log("No argument");
-}else if (arg2 ===undefined) {
-    console.log(`One argument: ${arg1}`);
+//if (arg1 === undefined) {
+ //   console.log("No argument");
+//}else if (arg2 ===undefined) {
+ //   console.log(`One argument: ${arg1}`);
+//} else {
+ //   console.log(`Two arguments:  ${arg1} ${arg2}`);
+
+//}
+
+// Get the first argument from the command line
+const firstArg = process.argv[2];
+
+// Check if an argument was given
+if (firstArg === undefined) {
+  console.log("No argument");
 } else {
-    console.log(`Two arguments:  ${arg1} ${arg2}`);
-
+  console.log(firstArg);
 }
+
 
